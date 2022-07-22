06bb871ffdc7ae6bc076928a9ddb906c4af7d4e9
ValeriaDiaz <valeriadiamon@gmail.com>
2022-07-21 19:51:40 -0500

    v.1 all issues solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container">
-    <a className="navbar-brand" href="#">NASA image of the day</a>
-  </div>
-</nav>
-
+      <div className="container">
+        <a className="navbar-brand" href="#">NASA image of the day</a>
+        </div>
+    </nav>
   )
 }
 
